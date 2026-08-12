9946dadda15f554ee6ed5d83fa29f199118c6f03
WitcherxSN <shravan.naik30@gmail.com>
2026-08-12 23:57:16 +0530

    server updated

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
+const Product = require("./models/Product");
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
 
@@ -6,6 +9,47 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("ShoppyGlobe API is running");
+});
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected successfully");
+  })
+  .catch((error) => {
+    console.log("MongoDB connection error:", error);
+  });
+
+  app.get("/products", async (req, res) => {
+  try {
+    const products = await Product.find();
+
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({
+      message: "Error fetching products"
+    });
+  }
+});
+
+app.get("/products/:id", async (req, res) => {
+  try {
+
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+      return res.status(404).json({
+        message: "Product not found"
+      });
+    }
+
+    res.status(200).json(product);
+
+  } catch (error) {
+    res.status(400).json({
+      message: "Invalid product ID"
+    });
+  }
 });
 
 app.listen(3000, () => {
